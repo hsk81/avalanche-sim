b8e762e586a2e110fc26f90aa4173c2c08f6f8b2
Hasan Karahan <hasan.karahan@blackhan.com>
2021-05-26 01:35:57 +0300

    1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup (
     name='avalanche-sim',
-    version='1.2.0',
+    version='1.3.0',
     description='Avalanche consensus simulator',
     author='Hasan Karahan',
     author_email='avalanche@blackhan.com',
